a0b17c6c2ec15bf70c1ba720ad173df35975b5d3
Sergio Daniel Sigala Hernandez <sergio.sigalahrnd@uanl.edu.mx>
2023-06-30 12:03:20 -0600

    changes

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,10 @@
-# build_files.sh
-pip install -r requirements.txt
+# Build the project
+echo "Building the project..."
+python -m pip install -r requirements.txt
 
-# make migrations
-python3.9 manage.py migrate 
-python3.9 manage.py collectstatic
+echo "Make Migration..."
+python manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+
+echo "Collect Static..."
+python manage.py collectstatic --noinput --clear
